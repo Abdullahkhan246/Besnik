01c587589bd0230f7903f5773a821d31c38330f7
Abdullahkhan246 <anittawilson50@gmail.com>
2026-02-04 14:29:08 +0500

    change

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../pages/home/Home';
 import Navbar from "../components/navbar/Navbar";
 
